a290141f4814bdb9de02eba2afb2cab7733bfc94
leelizs <leolorenz159@gmail.com>
2024-11-08 18:25:37 -0300

    debugando e testando

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // Rota para criar o QR Code
-app.post("/api/create-qrcode", async (req, res) => {
+app.post("/create-qrcode", async (req, res) => {  // Rota alterada
   try {
     const { valor } = req.body;
     const valorFormatado = parseFloat(valor).toFixed(2);
@@ -35,7 +35,7 @@ app.post("/api/create-qrcode", async (req, res) => {
 });
 
 // Rota para consultar o status do pagamento
-app.get("/api/verificar-status/:txid", async (req, res) => {
+app.get("/verificar-status/:txid", async (req, res) => {  // Rota alterada
   try {
     const { txid } = req.params;
     const statusData = await verificarStatusPagamento(txid);
